7b587fc0cfa52a56cb2a70bbf74872c12b565372
Kaushith_Gowda_M <koushith7@gmail.com>
2023-12-08 14:19:50 +0530

    Challenge#1

---
+++ Challenges/challenge-1/src/index.js
@@ -0,0 +1,59 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="jonas.jpeg" alt="jonas" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>
+        Jonas Schmedtmann
+      </h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return <div className="skill-list">
+    <Skill bg="blue" skill="React😎" />
+    <Skill bg="red" skill="Js🪄" />
+    <Skill bg="orange" skill="Html🔖" />
+    <Skill bg="yellow" skill="Sass🥰" />
+  </div>
+}
+
+function Skill(props) {
+  return <div className={`skill ${props.bg}`}>{props.skill}</div>
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
