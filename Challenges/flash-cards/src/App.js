e931967d8360a4fc28d7d8d9bda697127fcdbbc0
Kaushith_Gowda_M <koushith7@gmail.com>
2023-12-13 10:47:33 +0530

    flash cards

---
+++ Challenges/flash-cards/src/App.js
@@ -0,0 +1,59 @@
+import './index.css';
+import { useState } from 'react';
+
+function App() {
+  const [selectedId, setSelectedId] = useState('');
+  const questions = [
+    {
+      id: 3457,
+      question: 'What language is React based on?',
+      answer: 'JavaScript',
+    },
+    {
+      id: 7336,
+      question: 'What are the building blocks of React apps?',
+      answer: 'Components',
+    },
+    {
+      id: 8832,
+      question:
+        "What's the name of the syntax we use to describe a UI in React?",
+      answer: 'JSX',
+    },
+    {
+      id: 1297,
+      question: 'How to pass data from parent to child components?',
+      answer: 'Props',
+    },
+    {
+      id: 9103,
+      question: 'How to give components memory?',
+      answer: 'useState hook',
+    },
+    {
+      id: 2002,
+      question:
+        'What do we call an input element that is completely synchronised with state?',
+      answer: 'Controlled element',
+    },
+  ];
+
+  return (
+    <div className="App">
+      <div className="flashcards">
+        {questions.length > 0 &&
+          questions.map((item) => (
+            <div
+              className={selectedId === item.id ? 'selected' : ''}
+              onClick={() => setSelectedId(item.id)}
+              key={item.id}
+            >
+              {selectedId === item.id ? item.answer : item.question}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
