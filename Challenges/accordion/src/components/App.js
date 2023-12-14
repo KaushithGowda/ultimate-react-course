acc01caa3b488ad16248697ce53aed0097f8e062
Kaushith_Gowda_M <koushith7@gmail.com>
2023-12-14 15:45:43 +0530

    Accordion Component(v1)

---
+++ Challenges/accordion/src/components/App.js
@@ -0,0 +1,58 @@
+import '../index.css';
+import { useState } from 'react';
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data?.length > 0 &&
+        data.map((item, index) => (
+          <AccordionItem
+            title={item.title}
+            text={item.text}
+            index={index}
+            key={item.title}
+          />
+        ))}
+    </div>
+  );
+}
+
+function AccordionItem({ index, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      className={`item ${isOpen ? 'open' : ''}`}
+      onClick={() => setIsOpen((isOpen) => !isOpen)}
+    >
+      <span className="number">{index < 9 ? `0${index}` : index}</span>{' '}
+      <span className="title">{title}</span>
+      <span className="icon">{isOpen ? '-' : '+'}</span>
+      {isOpen && <p className="content-box">{text}</p>}
+    </div>
+  );
+}
+
+export default App;
