4e4c3c23246866a9fc0b58519af10f82b57789c8
Kaushith_Gowda_M <koushith7@gmail.com>
2023-12-26 16:30:06 +0530

    Implementing Main Pages and Routes

--- 11-worldwise/starter/world-wise/src/App.jsx
+++ 11-worldwise/starter/world-wise/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
