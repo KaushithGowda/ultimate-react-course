95abad9a601267c9ff6083032adfc5161c1c976a
Kaushith_Gowda_M <koushith7@gmail.com>
2023-12-26 19:14:50 +0530

    Linking between Routes With <Link /> and <NavLink />

---
+++ 11-worldwise/starter/world-wise/src/components/PageNav.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+
+function PageNav() {
+  return (
+    <>
+      <ul>
+        <li>
+          <NavLink to={'/'}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/pricing'}>Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/product'}>Product</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/page-not-found'}>Page Not Found❗️</NavLink>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default PageNav;
