7aee1fa0c6065b6220c22b083fc0de2afa031dae
Kaushith_Gowda_M <koushith7@gmail.com>
2024-01-03 23:09:57 +0530

    Working With Action Creators

--- 15-redux-intro/starter/redux-intro/src/store.js
+++ 15-redux-intro/starter/redux-intro/src/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
 
+// 262. Creating a Reducer: Bank Account
 const initialState = {
   balence: 0,
   loan: 0,
@@ -37,22 +38,59 @@ function reducer(state = initialState, action) {
   }
 }
 
+// 263. Creating a Redux Store
 const store = createStore(reducer);
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
-console.log(store.getState());
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// console.log(store.getState());
 
-store.dispatch({ type: 'account/withdraw', payload: 200 });
-console.log(store.getState());
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// console.log(store.getState());
 
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: {
-    amount: 1000,
-    purpose: 'Car',
-  },
-});
-console.log(store.getState());
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: {
+//     amount: 1000,
+//     purpose: 'Car',
+//   },
+// });
+// console.log(store.getState());
 
-store.dispatch({ type: 'account/payLoan' });
-console.log(store.getState());
+// store.dispatch({ type: 'account/payLoan' });
+// console.log(store.getState());
+
+// 264. Working with Action Creators
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: {
+      amount,
+      purpose,
+    },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+function logState() {
+  console.log(store.getState());
+}
+
+store.dispatch(deposit(500));
+logState();
+store.dispatch(withdraw(200));
+logState();
+store.dispatch(requestLoan(1000, 'car'));
+logState();
+store.dispatch(payLoan());
+logState();
