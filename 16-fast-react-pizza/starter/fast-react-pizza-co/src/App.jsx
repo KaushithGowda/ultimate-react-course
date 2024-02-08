cde04b166f62b42a48916f69c27ff172eb73c942
Kaushith_Gowda_M <koushith7@gmail.com>
2024-02-08 18:24:46 +0530

    Writing Data With React Router 'Actions'

--- 16-fast-react-pizza/starter/fast-react-pizza-co/src/App.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza-co/src/App.jsx
@@ -3,7 +3,9 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 import { loader as orderLoader } from './ui/Search';
@@ -30,6 +32,7 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
